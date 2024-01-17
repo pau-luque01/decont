a73b4fddc897e016b784254b8a0f89dc9cb8b146
Paula Leal Luque <paula@paulaleal.es>
2024-01-17 17:06:56 +0100

    Editar merge_fastqs.sh

--- scripts/merge_fastqs.sh
+++ scripts/merge_fastqs.sh
@@ -3,3 +3,16 @@
 # stored in the output directory specified by the second argument ($2).
 #
 # The directory containing the samples is indicated by the first argument ($1).
+
+if ["$#" -ne 3];then
+	echo 'Usage: $0 <samples_directory><output_directory><sample_id>'
+	exit 1
+fi
+
+samples_directory="$1"
+output_directory="$2"
+sample_id="$3"
+
+#Merging compressed text files
+cat '$samples_directory/$sample_id'*.fastq.gz > '$output_directory/$sample_id.fastq.gz'
+
