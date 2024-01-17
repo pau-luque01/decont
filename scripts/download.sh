3ad6c03b2c260a6de22c44b9985c9ad47878aa0f
Paula Leal Luque <paula@paulaleal.es>
2024-01-17 16:46:47 +0100

    Editar download.sh

--- scripts/download.sh
+++ scripts/download.sh
@@ -16,3 +16,31 @@
 #   CCAGGATTTACAGACTTTAAA
 #
 #   If $4 == "another" only the **first two sequence** should be output
+
+#carpeta vacía?
+if ['$#' -lt 2]; then
+	echo 'Usage: $0 <url> <output_directory> [y/n] [filtered_keyword]'
+	exit 1
+fi
+
+#Variables
+url = '$1'
+output_directory ='$2'
+filtered_key = '$3'
+uncompress = '$4'
+
+#Crear output_directory. SOLO si no exixte (-p)
+mkdir -p '$2'
+
+#Descargar el fichero
+wget -o '$2/$(basename '$1')' '$1'
+
+#Descomprimir el fichero
+if ['$4' == 'y'];then
+	gunzip '$2/$(basename '$1')'
+fi
+
+#Filtrar secuencia
+if [-n '$3'];then
+	grep -v '$3' '$2/$3(basename '$1')' > '$2/filtered_$(basename '$1')'
+fi
